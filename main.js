2510b04c49d0f031b42d3db6e73c82e2b4e8820c
SumitUmak58 <iamuksumit@gmail.com>
2024-10-06 00:51:12 +0530

    6-10 with responsive

--- main.js
+++ main.js
@@ -6,39 +6,4 @@ let typed = new Typed(".type-text", {
   loop: true,
 });
 
-
-// tab section
-
- 
-// Function to switch between tabs
-// function openTab(evt, tabName) {
-//     var i, tabcontent, tablinks;
-//     tabcontent = document.getElementsByClassName("tab-content");
-//     for (i = 0; i < tabcontent.length; i++) {
-//         tabcontent[i].style.display = "none";
-//     }
-//     tablinks = document.getElementsByClassName("tablink");
-//     for (i = 0; i < tablinks.length; i++) {
-//         tablinks[i].classList.remove("active");
-//     }
-//     document.getElementById(tabName).style.display = "block";
-//     evt.currentTarget.classList.add("active");
-// }
-
-// // Open the default tab on page load
-// document.getElementById("tab1").style.display = "block";
-// document.querySelector(".tablink.active").classList.add("active");
-
-// // Hide all tab contents except for the active tab on page reload
-// window.onload = function () {
-//     var activeTab = document.querySelector(".tablink.active");
-//     var activeTabName = activeTab.getAttribute("onclick").match(/'(.*?)'/)[1];
-//     var activeContent = document.getElementById(activeTabName);
-//     var allTabs = document.querySelectorAll('.tab-content');
-//     for (var i = 0; i < allTabs.length; i++) {
-//         if (allTabs[i].id !== activeTabName) {
-//             allTabs[i].style.display = "none";
-//         }
-//     }
-// };
  
